a7225b407bf960a4f293e92af0f67330b261e103
grazzgit <github@grazzhopper.maskmy.id>
2023-12-29 21:28:13 +0100

    feat: changed vimwiki to color wiki files and conform plugin file formatting

--- nvim/.config/nvim/lua/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/plugins/vimwiki.lua
@@ -19,19 +19,18 @@ return {
         path = "~/wiki/archives",
       },
     }
-    -- vim.g.vimwiki_global_ext = 0
+    vim.g.vimwiki_global_ext = 0
   end,
-  -- config = function()
-  -- 	vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  -- 		callback = function()
-  -- 			vim.cmd([[TaskWikiBufferLoad]])
-  -- 			vim.cmd("highlight VimwikiHeader1 guifg=#61afef ctermfg=81 gui=bold cterm=bold")
-  -- 			vim.cmd("highlight VimwikiHeader2 guifg=#56b6c2 ctermfg=81 gui=bold cterm=bold")
-  -- 			vim.cmd("highlight VimwikiHeader3 guifg=#98c379 ctermfg=114 gui=bold cterm=bold")
-  -- 			vim.cmd("highlight VimwikiHeader4 guifg=#e06c75 ctermfg=168 gui=bold cterm=bold")
-  -- 			vim.o.foldlevel = 999
-  -- 		end,
-  -- 		pattern = "*.wiki",
-  -- 	})
-  -- end,
+  config = function()
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+      callback = function()
+        vim.cmd("highlight VimwikiHeader1 guifg=#61afef ctermfg=81 gui=bold cterm=bold")
+        vim.cmd("highlight VimwikiHeader2 guifg=#56b6c2 ctermfg=81 gui=bold cterm=bold")
+        vim.cmd("highlight VimwikiHeader3 guifg=#98c379 ctermfg=114 gui=bold cterm=bold")
+        vim.cmd("highlight VimwikiHeader4 guifg=#e06c75 ctermfg=168 gui=bold cterm=bold")
+        vim.o.foldlevel = 999
+      end,
+      pattern = "*.wiki",
+    })
+  end,
 }
